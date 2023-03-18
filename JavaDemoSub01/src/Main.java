5119dbe35c8869eec9fc2fc46f4ac5eeef393478
dongerkang <18703560946@163.com>
2023-03-18 21:56:30 +0800

    第二次添加

---
+++ JavaDemoSub01/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author dek
+ * @date ${DATE}
+ * @description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
