ba16e6fd700e485908f028a94a60fa9e0662d788
dongerkang <18703560946@163.com>
2023-03-18 21:48:12 +0800

    首次添加文件

---
+++ JavaDemoSub01/src/User.java
@@ -0,0 +1,11 @@
+/**
+ * @author dek
+ * @date 2023-03-18
+ * @description
+ */
+public class User {
+    private String name;
+    private int age;
+    private String email;
+    private String address;
+}
